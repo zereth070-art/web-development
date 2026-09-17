1e88105e1ecb9e3de0135a4bb70c53b7a2850ba2
Zereth <zereth070@gmail.com>
2026-09-17 19:07:25 +0200

    react: unificar login/registro (LoginRegistro); pomodorozion: trabajo midgar; capturas y plantillas (fusion de copias locales)

---
+++ Aprender-DAW/01-Desarrollo-web-cliente/react/PCCOMPONENTES/src/Componentes/zonaCliente/login/LoginRegistro.jsx
@@ -0,0 +1,11 @@
+import "./LoginRegistro.css"
+
+function LoginRegistro() {
+  
+
+    return <div class="container">
+        <div class="row">
+            <img src="Aprender-DAW\01-Desarrollo-web-cliente\react\PCCOMPONENTES\public\logo-pccomponentes (1).svg"></img>
+        </div>
+    </div>
+}
